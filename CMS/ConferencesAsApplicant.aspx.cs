3c4226e070891be2067b55976a76b46ed7e24695
Yiğit Ersoy <80414632+yigitersoydev@users.noreply.github.com>
2024-03-22 05:27:32 +0300

    Update ConferencesAsApplicant.aspx.cs

--- CMS/ConferencesAsApplicant.aspx.cs
+++ CMS/ConferencesAsApplicant.aspx.cs
@@ -1,8 +1,15 @@
-﻿using System;using System.Collections.Generic;using System.Linq;using System.Web;using System.Web.UI;using System.Web.UI.WebControls;using System.Data.SqlClient;using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Windows;
 
 public partial class ConferencesAsApplicant : System.Web.UI.Page
 {
-    SqlConnection conn = new SqlConnection("Data Source=YIGIT\\SQLEXPRESS;Initial Catalog=CMS; integrated security = True; MultipleActiveResultSets = True; App=EntityFramework");
+    SqlConnection conn = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=CMS; integrated security = True; MultipleActiveResultSets = True; App=EntityFramework");
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -22,10 +29,19 @@ public partial class ConferencesAsApplicant : System.Web.UI.Page
         if (Session["USERSession"] != null)
         {
             //string usernamee = Session["USERSession"].ToString();
-            SqlCommand veriOkuma = new SqlCommand();            conn.Open();            veriOkuma.Connection = conn;
+            SqlCommand veriOkuma = new SqlCommand();
+            conn.Open();
+            veriOkuma.Connection = conn;
 
             veriOkuma.CommandText = ("SELECT C.[conferenceSubject],C.[date],C.[dueDate],C.[topic] FROM [dbo].[Conference] AS C INNER JOIN [dbo].[ConferenceUsers] AS CU ON CU.conferenceID = C.conferenceID WHERE [userID]='" + Session["USERSession"] + "'  "); //from olcak hangi tablodan ekiceÛini gšsterecek
-            SqlDataReader reader;            reader = veriOkuma.ExecuteReader();            rptr_ConferenceAsApplicant.DataSource = reader;            rptr_ConferenceAsApplicant.DataBind();            reader.Close();            conn.Close();
+            SqlDataReader reader;
+            reader = veriOkuma.ExecuteReader();
+
+            rptr_ConferenceAsApplicant.DataSource = reader;
+            rptr_ConferenceAsApplicant.DataBind();
+            reader.Close();
+
+            conn.Close();
         }
 
 
