8bb70e97609d0755901e46b4ac98224afa1ac184
Yiğit Ersoy <80414632+yigitersoydev@users.noreply.github.com>
2024-03-22 05:26:25 +0300

    Update AddAnnouncement.aspx.cs

--- CMS/AddAnnouncement.aspx.cs
+++ CMS/AddAnnouncement.aspx.cs
@@ -13,7 +13,7 @@ using System.Windows.Forms;
 
 public partial class AddAnnouncement : System.Web.UI.Page
 {
-    SqlConnection conn = new SqlConnection("Data Source=YIGIT\\SQLEXPRESS;Initial Catalog=CMS;Integrated Security=True;MultipleActiveResultSets=true");
+    SqlConnection conn = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=CMS;Integrated Security=True;MultipleActiveResultSets=true");
     protected void Page_Load(object sender, EventArgs e)
     {
         if (Session["USERSession"] == null)
